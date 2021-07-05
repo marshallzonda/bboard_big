3ff61bcf98ba53949c5553c1c5dea1b8a994f118
Bondarenko Vladislav <bond.zonda777@gmail.com>
2021-07-06 00:00:21 +0300

    registration

---
+++ bboard/main/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    index, other_page,
+    BBLoginView, profile,
+    BBLogoutView,
+    ChangeUserInfoView,
+    BBPasswordChangeView
+)
+app_name = 'main'
+urlpatterns = [
+    path('', index, name='index'),
+    path('<str:page>/', other_page, name='other'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/profile/change', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/password/change', BBPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+]
